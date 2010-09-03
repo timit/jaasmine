e9e4c044e33865645a71e7c9e0b15b737194b7b4
LogicLander <info@logiclander.com>
2010-09-03 02:06:09 +0000

    starting point for server code used to validate SPNego tokens

---
+++ jaasmine-parent/jaasmine-core/src/main/java/com/logiclander/jaasmine/SPNegoServer.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2010 LogicLander
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.logiclander.jaasmine;
+
+import static com.logiclander.jaasmine.JAASMineContants.*;
+
+import org.apache.commons.codec.binary.Base64;
+import org.ietf.jgss.GSSContext;
+import org.ietf.jgss.GSSCredential;
+import org.ietf.jgss.GSSException;
+import org.ietf.jgss.GSSManager;
+import org.ietf.jgss.Oid;
+
+/**
+ *
+ * @author tcarroll
+ */
+public class SPNegoServer {
+  private final GSSContext gssContext;
+  private byte[] delegateToken = null;
+
+  public SPNegoServer()
+         throws GSSException {
+    final Oid spnegoMechOid = new Oid(SPNEGO_MECH_OID);
+    final GSSManager gssManager = GSSManager.getInstance();
+
+    final GSSCredential gssServerCred = gssManager.createCredential(null, GSSCredential.DEFAULT_LIFETIME, spnegoMechOid, GSSCredential.ACCEPT_ONLY);
+    gssContext = gssManager.createContext(gssServerCred);
+  }
+
+  private void generateDelegateToken(String authenticationHeader)
+          throws GSSException {
+    byte[] token = Base64.decode(authenticationHeader);
+    delegateToken = gssContext.acceptSecContext(token, 0, token.length);
+  }
+
+  public boolean validateToken(String authenticationHeader)
+          throws GSSException {
+    generateDelegateToken(authenticationHeader);
+    if(gssContext.isEstablished()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public byte[] getDelegateToken() {
+    return delegateToken;
+  }
+}
